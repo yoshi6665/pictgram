8c2887c45d7311f7164fd14e86073f4c6122722a
EC2 Default User <ec2-user@ip-172-31-30-10.us-east-2.compute.internal>
2019-11-12 11:50:09 +0000

    pictgram

---
+++ app/models/user.rb
@@ -0,0 +1,9 @@
+class User < ApplicationRecord
+  validates :name, presence: true, length: {maximum:15}
+  
+  validates :password, format: {with: /\A(?=.*?[a-z])(?=.*?\d)[a-z\d]{8,32}+\z/i }
+  
+  validates :email, presence: true, format: {with: /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i}
+  has_secure_password
+end
+
---
+++ config/routes.rb
@@ -0,0 +1,11 @@
+Rails.application.routes.draw do
+ 
+  
+  get 'users/new'
+  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  root 'pages#index'
+  get 'pages/help'
+  
+  resources :users 
+
+end
